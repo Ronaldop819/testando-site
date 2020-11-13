81706b2cfa27e38e8f588d7ebdaf7aabac1f09bd
unknown <datascience@gmail.com>
2020-11-13 11:21:07 -0300

    new home

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,7 @@
 import { useState } from 'react';
 
 function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <Contador />
-            <h2>Test</h2>
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
-
-function Pagina() {
-    return <Contador />
+    return <h1>Home - alive</h1>
 }
 
 export default Home
